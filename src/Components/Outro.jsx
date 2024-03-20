bfdcdda25e2d46e300195ca1e65d7e0a2d0eecb9
ghady <ghadygergie0@gmail.com>
2024-03-20 23:24:57 +0200

    iamge fix

--- src/Components/Outro.jsx
+++ src/Components/Outro.jsx
@@ -2,7 +2,7 @@ function Outro() {
 	return (
 		<div className='outroContent '>
 			<div className='main-outro'>
-				<img src='outro.jpeg' alt='outro' />
+				<img src='header.jpeg' alt='outro' />
 				<h3 className='text '>Together Forever &#x1F5A4;</h3>
 			</div>
 		</div>
