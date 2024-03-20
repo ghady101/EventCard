dc3f0b6abb576bd2c5393d5c220093a59be40e9a
ghady <ghadygergie0@gmail.com>
2024-03-20 14:19:41 +0200

    test

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -2,9 +2,9 @@ function Intro() {
 	return (
 		<div className='content'>
 			<h1 className='title' style={{ marginBottom: '.9em' }}>
-				Elia
-				<br />&<br />
 				Tamara
+				<br />&<br />
+				Elia
 			</h1>
 			<h3 className='text'>Are getting married</h3>
 			<h3 className='swipe'>Swipe to continue</h3>
