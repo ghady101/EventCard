ddaebe46c2f4101be1bdf926cb91ebeeff16ae69
ghady <ghadygergie0@gmail.com>
2024-03-20 23:32:42 +0200

    line space

--- src/Components/Gift.jsx
+++ src/Components/Gift.jsx
@@ -6,9 +6,9 @@ function Gift() {
 			<h1 className='title' style={{ marginBottom: '.9em' }}>
 				Gift Registry
 			</h1>
-			<h3 className='text' style={{ margin: '24px' }}>
+			<h3 className='text' style={{ margin: '24px', lineHeight: '1.2em' }}>
 				Your love, laughter and company are all we could wish for on our special
-				day. However, if you would like to honor us with a gift a registry is
+				day. However, if you would like to honor us with a gift, a registry is
 				held at whish money
 				<br />
 				<br />
